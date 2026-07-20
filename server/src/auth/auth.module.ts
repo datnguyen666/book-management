3beaa43ad9752edb2f7d9c196bb591d81859c338
Nguyễn Tiến Đạt <198518849+datnguyen666@users.noreply.github.com>
2026-07-20 16:17:31 +0700

    feat: configure JwtModule and PassportModule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { StringValue } from 'ms';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+
+        signOptions: {
+          expiresIn: (configService.get<string>('JWT_EXPIRES_IN') ??
+            '1d') as StringValue,
+        },
+      }),
+    }),
+  ],
+
   controllers: [AuthController],
+
   providers: [AuthService],
+
+  exports: [JwtModule],
 })
 export class AuthModule {}

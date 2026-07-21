ad0558a98e5ffe501fa6c4fc983098361e76f782
Nguyễn Tiến Đạt <198518849+datnguyen666@users.noreply.github.com>
2026-07-21 13:56:39 +0700

    feat: implement POST /auth/login endpoint

--- server/src/auth/dto/login.dto.ts
+++ server/src/auth/dto/login.dto.ts
@@ -3,9 +3,9 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class LoginDto {
   @IsString()
   @IsNotEmpty()
-  username: string | undefined;
+  username!: string;
 
   @IsString()
   @IsNotEmpty()
-  password: string | undefined;
+  password!: string;
 }

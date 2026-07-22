a728c36088f679c77991942e8a1dd083d5c7af69
Nguyễn Tiến Đạt <198518849+datnguyen666@users.noreply.github.com>
2026-07-22 15:16:37 +0700

    implement POST /categories with auth guards

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+import { Role } from '@prisma/client';
+
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+
+@ApiTags('Categories')
 @Controller('categories')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+  @Post()
+  @ApiOperation({
+    summary: 'Create a new category',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Category created successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Category name already exists',
+  })
+  @ApiBearerAuth('JWT')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  async create(@Body() dto: CreateCategoryDto) {
+    return this.categoryService.create(dto);
+  }
+}
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreateCategoryDto) {
+    return await this.prisma.category.create({
+      data: dto,
+    });
+  }
+  catch(error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('Category already exists');
+      }
+    }
+    throw error;
+  }
+}

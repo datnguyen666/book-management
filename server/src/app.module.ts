a7a2c495d1f878c50cab9a1e273ab3e6bb6e2def
Nguyễn Tiến Đạt <198518849+datnguyen666@users.noreply.github.com>
2026-07-23 15:01:57 +0700

    serve static upload files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,15 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { BookModule } from './book/book.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',

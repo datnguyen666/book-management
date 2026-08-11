c62f363844fda5239f1903d22899c6be56f67d43
Nguyễn Tiến Đạt <198518849+datnguyen666@users.noreply.github.com>
2026-08-11 15:51:45 +0700

    Fix upload ảnh

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -4,9 +4,6 @@ import { useAuthStore } from "@/store/auth.store";
 
 export const api = axios.create({
   baseURL: "http://localhost:3000/api/v1",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 api.interceptors.request.use((config) => {

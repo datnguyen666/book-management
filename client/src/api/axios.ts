43a88519efa64bcd52cb222758a4b034e35fcde2
Nguyễn Tiến Đạt <198518849+datnguyen666@users.noreply.github.com>
2026-09-22 11:25:10 +0700

    Fix

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/auth.store";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: `${import.meta.env.VITE_API_ORIGIN}/api/v1`,
 });
 
 api.interceptors.request.use((config) => {
